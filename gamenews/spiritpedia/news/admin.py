62d33cfa30baec0cf8c8ae942f97f156262d1f40
FuZe46 <114906015+FuZe46@users.noreply.github.com>
2024-03-05 22:30:17 +0300

    Add files via upload

---
+++ gamenews/spiritpedia/news/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import News
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'summary', 'image', 'important')
+    list_filter = ('important',)
+    search_fields = ('title', 'summary')
