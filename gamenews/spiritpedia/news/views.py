60d00303137303f8e7f7e839b33e7a385aab7dc8
FuZe46 <114906015+FuZe46@users.noreply.github.com>
2024-03-05 10:39:18 +0300

    Add files via upload

--- gamenews/spiritpedia/news/views.py
+++ gamenews/spiritpedia/news/views.py
@@ -29,3 +29,6 @@ def upload_news(request):
         form = NewsForm()
     return render(request, 'upload_news.html', {'form': form})
 
+def news_detail(request, pk):
+    news_item = get_object_or_404(News, pk=pk)
+    return render(request, "news_detail.html", {'news_item': news_item})
--- gamenews/spiritpedia/spiritpedia/urls.py
+++ gamenews/spiritpedia/spiritpedia/urls.py
@@ -7,9 +7,11 @@ from news import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('news', views.news, name='news_list'),
     path('guids', views.guids, name='guids'),
     path('matches', views.matches, name='matches'),
     path('tournaments', views.tournaments, name='tournaments'),
     path('upload_news', views.upload_news, name='upload_news'),
+    path('news/<int:pk>/', views.news_detail, name='news_detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
