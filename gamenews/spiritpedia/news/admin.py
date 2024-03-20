255c2f5068e1c1720cd07f95656e7fe3771f63b3
Rumi010 <71969449+Rumi010@users.noreply.github.com>
2024-03-20 20:47:16 +0300

    Add files via upload

--- gamenews/spiritpedia/news/admin.py
+++ gamenews/spiritpedia/news/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Tournament, Team
 
-@admin.register(News)
+
+
+
+
+
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'summary', 'image', 'important')
     list_filter = ('important',)
     search_fields = ('title', 'summary')
+
+admin.site.register(News, NewsAdmin)
+
+class TournamentAdmin(admin.ModelAdmin):
+  list_display = ('image', 'name', 'organizer', 'start_time', 'end_time')
+admin.site.register(Tournament, TournamentAdmin)
+
+class TeamAdmin(admin.ModelAdmin):
+  list_display = ('logo', 'name', 'players')
+admin.site.register(Team, TeamAdmin)
+
--- gamenews/spiritpedia/news/models.py
+++ gamenews/spiritpedia/news/models.py
@@ -8,3 +8,24 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Tournament(models.Model):
+  """Модель турнира."""
+  image = models.ImageField(upload_to='tournaments')
+  name = models.CharField(max_length=255)
+  organizer = models.CharField(max_length=255)
+  start_time = models.DateTimeField()
+  end_time = models.DateTimeField()
+  teams = models.ManyToManyField('Team')
+
+  def __str__(self):
+    return self.name
+  
+class Team(models.Model):
+  """Модель команды."""
+  logo = models.ImageField(upload_to='teams')
+  name = models.CharField(max_length=255)
+  players = models.TextField()
+
+  def __str__(self):
+    return self.name
